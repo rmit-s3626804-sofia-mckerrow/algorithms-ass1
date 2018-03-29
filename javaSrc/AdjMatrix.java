10d2f32cf32a5854b5461bb271e5d9da57127bb3
rmit-s3626804-sofia-mckerrow <s3626804@student.rmit.edu.au>
2018-03-29 14:19:36 +1100

    Updated addVertex, addEdge and printEdges in AdjMatrix

--- javaSrc/AdjMatrix.java
+++ javaSrc/AdjMatrix.java
@@ -14,7 +14,10 @@ public class AdjMatrix<T extends Object> implements FriendshipGraph<T> {
 	private int maxVert; // maximum number of vertices
 	private int numVert; // number of vertices
 	private String[] vertexList;
+	private String[] edgeList;
 	private int[][] adjMatrix;
+	private int rowIndex; // index number for row in adjMatrix[][]
+	private int colIndex; // index number for column in adjMatrix[][]
 
 	/**
 	 * Contructs empty graph.
@@ -23,6 +26,7 @@ public class AdjMatrix<T extends Object> implements FriendshipGraph<T> {
 		maxVert = 4000;
 		numVert = 0;
 		vertexList = new String[maxVert];
+		edgeList = new String[maxVert];
 		adjMatrix = new int[maxVert][maxVert];
 
 		// Initialise all entries in adjacency matrix to zero
@@ -31,7 +35,7 @@ public class AdjMatrix<T extends Object> implements FriendshipGraph<T> {
 				adjMatrix[i][j] = 0;
 			}
 		}
-		
+
 	} // end of AdjMatrix()
 
 	public void addVertex(T vertLabel) {
@@ -39,24 +43,68 @@ public class AdjMatrix<T extends Object> implements FriendshipGraph<T> {
 		if (numVert < maxVert) {
 			// Check if vertex is already in list
 			for (int i = 0; i < vertexList.length; i++) {
-				if (vertexList[i] != null && vertexList[i].equals((String) vertLabel)) {
+				// check if vertLabel has already been added
+				if (Arrays.asList(vertexList).contains(vertLabel)) {
 					System.err.println("Vertex has already been added");
 					break;
 				}
-				// If index i already has a vertex move onto next iteration of for loop
+				// if index i already has a vertex move onto next iteration of for loop
 				else if (vertexList[i] != null)
 					continue;
-				// If index i is empty add vertex
+				// if index i is empty add vertLabel as vertex
 				else {
 					vertexList[i] = (String) vertLabel;
 					numVert++;
 					break;
 				}
-			}				
+			}
 		}
 	} // end of addVertex()
 
 	public void addEdge(T srcLabel, T tarLabel) {
+		boolean vertexFound = false;
+
+		// check if vertex srcLabel has already been added
+		for (int i = 0; i < vertexList.length; i++) {
+			if (Arrays.asList(vertexList).contains(srcLabel)) {
+				rowIndex = Arrays.asList(vertexList).indexOf(srcLabel);
+				vertexFound = true;
+				break;
+			}
+		}
+		if (!vertexFound)
+			System.err.println("Vertex " + srcLabel + " has not been added");
+		
+		// reset vertexFound to false before next check
+		vertexFound = false;
+
+		// check if vertex tarLabel has already been added
+		for (int i = 0; i < vertexList.length; i++) {
+			if (Arrays.asList(vertexList).contains(tarLabel)) {
+				colIndex = Arrays.asList(vertexList).indexOf(tarLabel);
+				vertexFound = true;
+				break;
+			}
+		}
+		if (!vertexFound)
+			System.err.println("Vertex " + tarLabel + " has not been added");
+		
+		// Check if vertexList contains both srcLabel and tarLabel
+		if (Arrays.asList(vertexList).contains(srcLabel) && Arrays.asList(vertexList).contains(tarLabel)) {
+			// Add edge to edgeList
+			for (int i = 0; i < edgeList.length; i++) {
+				if (edgeList[i] != null)
+					continue;
+				else {
+					edgeList[i] = (String) srcLabel + " " + (String) tarLabel;
+					break;
+				}
+			}
+			
+			// Add edge to adjMatrix
+			adjMatrix[rowIndex][colIndex] = 1;
+			adjMatrix[colIndex][rowIndex] = 1;
+		}
 
 	} // end of addEdge()
 
@@ -78,18 +126,26 @@ public class AdjMatrix<T extends Object> implements FriendshipGraph<T> {
 
 	public void printVertices(PrintWriter os) {
 		os = new PrintWriter(System.out, true);
-		
+
 		for (int i = 0; i < vertexList.length; i++) {
-			if (vertexList[i] != null ) {
-				os.print(vertexList[i]+ " ");
+			if (vertexList[i] != null) {
+				os.print(vertexList[i] + " ");
 			}
 		}
-		
+
 		os.flush();
 	} // end of printEdges()
 
 	public void printEdges(PrintWriter os) {
-		// Implement me!
+		os = new PrintWriter(System.out, true);
+
+		for (int i = 0; i < edgeList.length; i++) {
+			if (edgeList[i] != null) {
+				os.println(edgeList[i]);
+			}
+		}
+
+		os.flush();
 	} // end of printEdges()
 
 	public int shortestPathDistance(T vertLabel1, T vertLabel2) {
